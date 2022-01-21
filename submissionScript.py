fbed22444da4e407230bcdb455cff708686d551e
srmckee99 <sam_mckee@outlook.com>
2022-01-20 19:17:04 -0500

    Modify script to use grequests for asynch requests

--- submissionScript.py
+++ submissionScript.py
@@ -1,18 +1,21 @@
 import json
-import requests
+#import requests
+import grequests
 
 numMessages = 10 #change this to modify number of message sent
 url = "http://192.168.1.1/formSubmit.json"
 ID = "ADC1" #if you want to change id, you can
 
 i = 0 
-values = { "clientID" : ID,
-            "message" : "Edge isn't that bad"}
+values = [{ "clientID" : ID,
+            "message" : "Edge isn't that bad"}]
 
-while(i<numMessages):
-    values["message"] = "message number " + str(i)
-    r = requests.post(url,params=values)
-    print(f"Status Code: {r.status_code}")
-    response = r.text
-    print("Reponse:" + response)
-    i+=1
+for i in range(numMessages):
+    values.append({"clientID": ID,
+                    "message" : "message number" + str(i)})
+
+    
+
+
+rs = (grequests.post(url,params=p) for p in values)
+print(grequests.map(rs))
