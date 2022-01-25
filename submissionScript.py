41c5f654a3d9a6324d8b23897c3056a87ae53b31
srmckee99 <sam_mckee@outlook.com>
2022-01-25 18:46:46 -0500

    testing with clients sending multiple messages

--- submissionScript.py
+++ submissionScript.py
@@ -1,21 +1,23 @@
 import json
-#import requests
 import grequests
 
-numMessages = 10 #change this to modify number of message sent
-url = "http://192.168.1.1/formSubmit.json"
+"""___________ Configuration ___________"""
+numClients = 1 #change this to modify number of clients 
+numMessages = 10 #Change this to modify number of messages sent by each client
 ID = "ADC1" #if you want to change id, you can
 
-i = 0 
+
+url = "http://192.168.1.1/formSubmit.json"
+
+i = 0
+j = 0
 values = [{ "clientID" : ID,
             "message" : "Edge isn't that bad"}]
 
 for i in range(numMessages):
-    values.append({"clientID": ID,
-                    "message" : "message number" + str(i)})
+    for j in range(numClients):
+        values.append({"clientID": ID,
+                        "message" : "message number" + str(j)})
 
-    
-
-
-rs = (grequests.post(url,params=p) for p in values)
-print(grequests.map(rs))
+    rs = (grequests.post(url,params=p) for p in values)
+    print(grequests.map(rs))
