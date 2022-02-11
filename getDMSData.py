c25177e7ec6898a7e1a8f0cd26bf200d7c07fac3
srmckee99 <sam_mckee@outlook.com>
2022-02-11 11:58:20 -0500

    Added get DMS data, and everything is working as expected?

--- getDMSData.py
+++ getDMSData.py
@@ -1,17 +1,67 @@
 import json
 import requests
 import getpass
+import re
+import time
+from submissionScript import *
 
-url = "https://owl-dms-api.us-south.cf.appdomain.cloud/api/users/authenticate"
-username = input("Enter DMS username: ")
-password = getpass.getpass("Enter DMS password: ")
+"""___________ Configuration ___________"""
+actualOutput = "actual.out"
 
-#Put info into JSON payload
-payload = { "username" : username,
-            "password" : password}
+def writeToFile(dataRet):
+    dataOut = open(actualOutput,"w")
+    counter = 0
 
-ret = requests.post(url, json=payload)
-jsonRet = ret.json()
+    for element in dataRet.json():
+        #first check to be sure that the event is not a status update (we dont care about those rn)
+        eventType = element["eventType"]
+        if(eventType!="status"):
+            payload = re.split(',|{|{',element["payload"])
+            message = re.split(':',payload[3])[1][7:-2]
+            dataOut.write("Message:\t"+message+"\n")
 
+            
+            
 
-print(jsonRet["token"])
+def getData(startTime,endTime):
+    tokenUrl = "https://owl-dms-api.us-south.cf.appdomain.cloud/api/users/authenticate"
+    fileOutput = "actualOut"
+    dataOut = open(fileOutput,"w")
+    """
+    username = input("Enter DMS username: ")
+    password = getpass.getpass("Enter DMS password: ")
+    duckID = input("Enter Papa Duck ID: ")
+    """
+    #Hardcoded for testing
+    username = input("Enter DMS Username: ")
+    password = getpass.getpass("Enter DMS password: ")
+    duckID = "CalPolyPapa001"
+
+    # startTime = 1581234722
+    # endTime = 1644393122
+
+    #Put info into JSON payload
+    payload = { "username" : username,
+                "password" : password}
+
+    #Parse for the return for the token value
+    ret = requests.post(tokenUrl, json=payload)
+    jsonRet = ret.json()
+    token = jsonRet["token"]
+
+    dataUrl = "https://owl-dms-api.us-south.cf.appdomain.cloud/api/userdata/getrawdata"
+
+    #hopefully make so that user can just run the script and this is auto filled
+    timeAppend = "?start="+str(startTime)+"&end="+str(endTime)+"&papaId="+duckID    
+
+    #create the header to contain the token
+    tokenHeader = { 'Authorization' : 'Bearer '+token}
+
+    #get the data?
+    dataRet = requests.get(dataUrl+timeAppend,headers=tokenHeader)
+
+    print("response: "+str(dataRet.status_code) +" and raw: " +str(dataRet.raw))
+    #print(dataRet.json())
+    dataOut.write(str(dataRet.json()))
+
+    writeToFile(dataRet)
